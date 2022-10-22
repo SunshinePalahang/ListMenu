a820fe6f96f254fd4402ccb90eac7462a35bdd42
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2022-10-22 13:46:30 +0800

    nested list

---
+++ AsteriskName.py
@@ -0,0 +1,8 @@
+#Create a program that will print your nickname using only asterisk character (*)
+from re import U
+
+
+nickname ="SUN"
+s = [[" " for i in range(6)] for j in range(7)]
+u = [[" " for i in range(6)] for j in range(7)]
+n = [[" " for i in range(6)] for j in range(7)]
