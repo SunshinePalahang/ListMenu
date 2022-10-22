d008d9f84b4a4a4afb76d9f05371fda4cad6dccc
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2022-10-22 13:57:55 +0800

    printing Asterisk Name w/ color

--- AsteriskName.py
+++ AsteriskName.py
@@ -1,4 +1,6 @@
 #Create a program that will print your nickname using only asterisk character (*)
+from colorama import Fore
+
 
 nickname ="SUN"
 s = [[" " for i in range(6)] for j in range(7)]
@@ -23,3 +25,14 @@ for row in range(7):
     for col in range (6):
         if col==0 or col==5 or (row==col and (col>0 and col<5)):
             n [row][col]= "*"
+
+for i in range (7):
+    for j in range (6):
+        print(s[i][j], end="")
+    print(end="  ")
+    for j in range (6):
+        print(u[i][j], end="")
+    print(end="  ")
+    for j in range (6):
+            print(n[i][j], end="")
+    print(Fore.LIGHTYELLOW_EX)
